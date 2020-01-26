a3032db25552d5432cfdbe5d702a39d6976fd2bd
abcl0653 <fd.cailin@gmail.com>
2020-01-26 23:10:37 +0800

    formatter

--- ui5-walkthrough/webapp/controller/InvoiceList.controller.js
+++ ui5-walkthrough/webapp/controller/InvoiceList.controller.js
@@ -1,7 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
-], (Controller, JSONModel) => Controller.extend("sap.ui.demo.walkthrough.InvoiceList", {
+    "sap/ui/model/json/JSONModel",
+    "../model/formatter"    
+], (Controller, JSONModel, formatter) => Controller.extend("sap.ui.demo.walkthrough.InvoiceList", {
+        formatter: formatter,
         onInit: function() {
             let oViewModel = new JSONModel({currency:"EUR"})
             this.getView().setModel(oViewModel,"view")
